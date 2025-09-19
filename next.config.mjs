2d8ac129fc0f76f4d022b5f9862ce95f33b7b0dc
21sean <21sean@users.noreply.github.com>
2025-09-18 18:50:28 -0700

    chore: add basePath and assetPrefix to Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/photos',
+  assetPrefix: '/photos',
   images: {
     unoptimized: true,
     domains: ['picsum.photos', 'cdn.myportfolio.com']
