69cae6ad207b3b43e37136915dd3552bc16923ab
21sean <21sean@users.noreply.github.com>
2025-09-18 18:09:33 -0700

    Add point-in-polygon-hao dependency and update Next.js configuration for image handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: [
+    'react-globe.gl',
+    'globe.gl',
+    'three-globe',
+    'three-conic-polygon-geometry',
+    '@turf/boolean-point-in-polygon'
+  ],
   images: {
     domains: ['picsum.photos', 'cdn.myportfolio.com']
   }
