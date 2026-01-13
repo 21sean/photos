88cbeee232c3565f3b8abb497ec733167f049d85
S P <smp2017@gmail.com>
2026-01-12 19:01:05 -0800

    Add tailwind-scrollbar dependency and implement custom scrollbar styles for album pages
    - Introduced the tailwind-scrollbar package to enhance scrollbar styling.
    - Updated tailwind.config.ts to include the scrollbar plugin.
    - Added custom scrollbar styles in globals.css for album pages, improving aesthetics and user experience.
    - Modified AlbumPageClient component to apply new scrollbar styles for better visibility and interaction.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import scrollbar from 'tailwind-scrollbar';
 
 const config: Config = {
   content: [
@@ -25,6 +26,8 @@ const config: Config = {
       }
     }
   },
-  plugins: []
+  plugins: [
+    scrollbar({ nocompatible: true })
+  ]
 };
 export default config;
