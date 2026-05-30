f511c0f09dfeb6beafea5bb6577d91fa91d7dea7
S P <smp2017@gmail.com>
2026-05-30 13:52:27 -0700

    Prune dead CSS and tighten build config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,6 @@ import scrollbar from 'tailwind-scrollbar';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/lib/**/*.{js,ts,jsx,tsx,mdx}'
   ],
